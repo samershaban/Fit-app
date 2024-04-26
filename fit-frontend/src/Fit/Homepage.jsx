268277319edcb6d0bf654ef6531e76ed1fb0863e
samershaban <samershaban11@gmail.com>
2024-04-26 09:36:24 -0400

    updates

--- fit-frontend/src/Fit/Homepage.jsx
+++ fit-frontend/src/Fit/Homepage.jsx
@@ -7,14 +7,29 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import { FilledInput, InputAdornment, MenuItem, Select, TextField } from '@mui/material';
+import FormHelperText from '@mui/material/FormHelperText';
+
+import {IMaskInput} from 'react-imask';
+import { NumericFormat } from 'react-number-format';
+import PropTypes from 'prop-types';
+import Stack from '@mui/material/Stack';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
 
 const steps = ['Goals', 'Basic Info', 'Routine'];
 
+// Homepage Component
 export const Homepage = () => {
 
   const { authState } = useOktaAuth();
 
-  //asdf
+  //active step
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
 
@@ -60,6 +75,18 @@ export const Homepage = () => {
     setActiveStep(0);
   };
 
+  // info
+  const [height, setHeight] = React.useState('');
+  const [weight, setWeight] = React.useState('');
+
+  const handleChangeHeight = (event) => {
+    setHeight(event.target.value);
+  };
+
+  const handleChangeWeight = (event) => {
+    setWeight(event.target.value);
+  };
+
   return(
     <div className="container mt-3">
       <h1>Welcome to Fit App</h1>
@@ -100,6 +127,18 @@ export const Homepage = () => {
       {activeStep === 0 ? (
         <React.Fragment>
           <Typography sx={{ mt: 2, mb: 1 }}>Step 1: What are your goals?</Typography>
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Goals</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="female"
+              name="radio-buttons-group"
+            >
+              <FormControlLabel value="Loose" control={<Radio />} label="Loose Weight" />
+              <FormControlLabel value="Maintain" control={<Radio />} label="Maintain Weight" />
+              <FormControlLabel value="Gain" control={<Radio />} label="Gain Lean Weight" />
+            </RadioGroup>
+          </FormControl>
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
             <Button
               color="inherit"
@@ -126,6 +165,59 @@ export const Homepage = () => {
       {activeStep === 1 ? (
         <React.Fragment>
           <Typography sx={{ mt: 2, mb: 1 }}>Step 2: We need some basic info</Typography>
+          <Stack direction="row" spacing={0}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Height</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                size='2'
+                sx={{ m: 1, width: '25ch' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="start">lbs</InputAdornment>,
+                }}
+                value={height}
+                label="Height"
+                onChange={handleChangeHeight}
+              >
+                <MenuItem value={4}>4 ft</MenuItem>
+                <MenuItem value={5}>5 ft</MenuItem>
+                <MenuItem value={6}>6 ft</MenuItem>
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Height</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                size='2'
+                sx={{ m: 1, width: '25ch' }}
+                InputProps={{
+                  endAdornment: <InputAdornment position="start">lbs</InputAdornment>,
+                }}
+                value={height}
+                label="Height"
+                onChange={handleChangeHeight}
+              >
+                <MenuItem value={0}>0 in</MenuItem>
+                <MenuItem value={1}>1 in</MenuItem>
+                <MenuItem value={2}>2 in</MenuItem>
+                <MenuItem value={3}>3 in</MenuItem>
+              </Select>
+            </FormControl>
+          </Stack>
+          <br/>
+          <TextField
+            label="Weight"
+            id="outlined-start-adornment"
+            size='2'
+            sx={{ m: 1, width: '25ch' }}
+            InputProps={{
+              endAdornment: <InputAdornment position="start">lbs</InputAdornment>,
+            }}
+          />
+          
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
             <Button
               color="inherit"
