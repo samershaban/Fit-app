de43e0d77a7619695d1ce68757c4809a266a38be
samershaban <samershaban11@gmail.com>
2024-06-03 07:52:51 -0400

    updates

--- fit-frontend/src/Fit/DailyRoutine.js
+++ fit-frontend/src/Fit/DailyRoutine.js
@@ -21,7 +21,7 @@ export class DailyRoutine {
 
   createRoutine = () => {
     const total = this.bodys.length;
-
+    
     for(let b=0;b<this.bodys.length;b++){
       // console.log(bodys[i]);
       let body = this.bodys[b];
