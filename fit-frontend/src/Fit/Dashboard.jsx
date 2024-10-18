6a84a0697ce918f98c2b7b10cea7004d0e3cb844
samershaban <samershaban11@gmail.com>
2024-10-18 12:11:35 -0400

    weight updates

--- fit-frontend/src/Fit/Dashboard.jsx
+++ fit-frontend/src/Fit/Dashboard.jsx
@@ -1,27 +1,30 @@
 import { useOktaAuth } from "@okta/okta-react";
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
-
+import { LineChart } from '@mui/x-charts/LineChart';
 import './Dashboard.css'
+import { Button, Grid, Card, Paper, InputAdornment, TextField, Typography } from "@mui/material";
 
 export const Dashboard = () => {
 
   const {authState} = useOktaAuth();
   const [notes, setNotes] = useState([]);
+  const [weights, setWeights] = useState([]);
   const [selectedNote, setSelectedNote] = useState(-1);
   const [selectedNoteBody, setSelectedNoteBody]= useState('');
-  const [selectedNoteTitle, setSelectedNoteTitle]= useState('');
-
+  const [selectedNoteTitle, setSelectedNoteTitle]= useState(0);
+  const [weight, setWeight] = useState(0);
   // const app_url = 'fit.app';
   const app_url = 'http://localhost:8080';
 
   const bodyRef = useRef(null);
 
  useEffect(() => {
-  const fetchNotes = async () => {
-    console.log(authState);
+
+  const fetchWeights = async () => {
+    // console.log(authState);
     if (authState && authState?.isAuthenticated) {
-      const url = `${app_url}/api/notes/byUserEmail`;
+      const url = `${app_url}/api/weights/byUserEmail`;
       const requestOptions = {
         method: 'GET',
         headers: {
@@ -30,30 +33,42 @@ export const Dashboard = () => {
         }
       };
       axios.get(url, requestOptions).then((res) =>{
-        console.log(res.data);
-        setNotes(res.data);
+        // console.log(res.data);
+        setWeights(res.data.sort((a,b) => b.date.localeCompare(a.date)));
+        setWeight(weights[0].value);// fix
       }).catch(err => {
         console.log(err);
       })
     }
   }
-  fetchNotes();
+  fetchWeights();
+
  }, [authState])
 
- // when selected note changes
+
  useEffect(() => {
-  console.log(notes);
- }, [notes])
+  console.log(weights);
+ }, [weights])
 
+ useEffect(() => {
+  console.log(weight);
+ }, [weight])
 
- const handleTextChange = (e) => {
-  setSelectedNoteBody(e.target.value);
-}
+  const handleChangeWeight = (e) => {
+    if(e.target.value < 0){
+      (e.target.value = 0)
+    }
+    setWeight(e.target.value);
+  };
 
-const handleTitleTextChange = (e) => {
-  console.log(e.target.value);
-  setSelectedNoteTitle(e.target.value);
-}
+  const handleTextChange = (e) => {
+    setSelectedNoteBody(e.target.value);
+  }
+
+  const handleTitleTextChange = (e) => {
+    console.log(e.target.value);
+    setSelectedNoteTitle(e.target.value);
+  }
 
 
   const selectNote = (i) => {
@@ -61,6 +76,34 @@ const handleTitleTextChange = (e) => {
     setSelectedNoteBody(notes[i].body);
     setSelectedNoteTitle(notes[i].title);
     // console.log(selectedNote);
+  }
+
+  const addWeight = () => {
+    const url = `${app_url}/api/weights/byUserEmail`;
+      const requestOptions = {
+        method: 'POST',
+        url: url,
+        headers: {
+          Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+          'Content-Type': 'application/json',
+        },
+        data: {
+          "value": weight,
+          "date": Date.now()
+        }
+      };
+
+      axios(requestOptions)
+      .then((res) =>{
+        // console.log(res.data);
+        let currentNotes = notes;
+        currentNotes.unshift(res.data);
+        setNotes(prevNote => [...currentNotes]);
+        document.getElementById("list-home-list").click();
+        // selectNote(notes.length-1);
+      }).catch(err => {
+        console.log(err);
+      })
   }
 
   const addNewNote = () => {
@@ -152,26 +195,64 @@ const handleTitleTextChange = (e) => {
     return (
     <div className="container mt-3">
       <h1>Welcome to Fit App</h1>
-      <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <div className="list-group" id="list-tab" role="tablist">
-            {notes.length == 0? <div className="list-group-item list-group-item-action disabled">No Routine</div>:
-            notes.map((note, i) => (
-              <a key={i} onClick={() => {selectNote(i)}} className="list-group-item list-group-item-action" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">
-                <h3>{note.title}</h3>
-                <p>{note.body.length<35?note.body:note.body.substring(0,33)+".."}</p>
-              </a>
-            ))}
-            </div>
+      <Grid container spacing={0}>
+        <Grid item xs={6}>
+          <Paper variant="outlined" margin={5}>
+          <Typography>Weight</Typography>
+          <br/>
+          <TextField
+            label="Weight"
+            // id="outlined-start-adornment"
+            id="demo-simple-select-label"
+            value={weight}
+            size='2'
+            sx={{ m: 1, width: '22.2ch' }}
+            onKeyDown={(evt) => ["e", "E", "+", "-", "."].includes(evt.key) && evt.preventDefault()}
+            inputProps={{
+              endadornment: <InputAdornment position="start">lbs</InputAdornment>,
+              type: 'number',
+              min: 0,
+              max: 999,
+              length: 3
+            }}
+            onChange={handleChangeWeight}
+          />
+          <button onClick={addWeight} type="button" className="btn btn-primary">Check in Weight</button>
+          <div>
+          <LineChart
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+              {
+                data: [2, 5.5, 2, 8.5, 1.5, 5],
+              },
+            ]}
+            width={500}
+            height={300}
+          />
           </div>
-          <div className="col-4" style={{paddingBottom: "5px"}}>
-            Coming soon
+          <div className="list-group" id="list-tab" role="tablist">
+          {weights.length == 0? <div className="list-group-item list-group-item-action disabled">No Weight entrees</div>:
+          weights.map((w, i) => (
+            <a key={i} onClick={() => {/*selectNote(i)*/}} className="list-group-item list-group-item-action" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">
+              <h3>{w?.value}</h3>
+              <p>{w?.date}</p>
+            </a>
+          ))}
           </div>
-          <div className="col-4" style={{paddingBottom: "5px"}}>
-            Your diet
-          </div>
-          {/* <div className="col-8" style={{paddingBottom: "5px"}}>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+        <Paper variant="outlined" margin={5}>
+          <Typography>Diet</Typography>
+        </Paper>
+        </Grid>
+        <Grid item xs={12}>
+        <Paper variant="outlined" margin={5}>
+          <Typography>Routine</Typography>
+        </Paper>
+        <>
+          {
+          /* <div className="col-8" style={{paddingBottom: "5px"}}>
           <div className="btn-group" role="group" aria-label="Basic mixed styles example">
             <button onClick={addNewNote} type="button" className="btn btn-primary">Add</button>
             <button onClick={updateNote} type="button" className="btn btn-secondary">Save</button>
@@ -200,9 +281,11 @@ const handleTitleTextChange = (e) => {
                 // style={{width: '100%', height: '100%', resize: 'none'}}
                 />
           </div>
-          </div> */}
-        </div>
-      </div>
+          </div> */
+          
+          }</>
+        </Grid>
+      </Grid>
       
 
     </div>)
